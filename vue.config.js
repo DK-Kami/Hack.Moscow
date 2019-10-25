a0bfaad8b2f5012ca089a4e04d0701430c609c1c
Kami San <1.dankon.1@gmail.com>
2019-10-25 13:21:16 +0300

    add ssl sertificate and update gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+const fs = require('fs');
+
+if (process.env.NODE_ENV === 'development') {
+  module.exports = {
+    devServer: {
+      host: 'vdooh.dev',
+      port: 8080,
+      https: {
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT)
+      }
+    },
+
+    configureWebpack: {
+      devtool: 'source-map'
+    },
+    transpileDependencies: [
+      'vuetify',
+    ],
+
+    productionSourceMap: false
+  }
+}
+else {
+  module.exports = {
+    transpileDependencies: [
+      'vuetify',
+    ],
+  };
 }
