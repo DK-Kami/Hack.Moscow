edfcb9eb5c960b8e4ca1f07a7aa7494e766000d3
Kami San <1.dankon.1@gmail.com>
2019-10-26 15:58:02 +0300

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,11 @@ const routes = [
     component: () => import('@/views/ComparePage'),
   },
   {
+    name: 'DeputatPage',
+    path: '/deputats',
+    component: () => import('@/views/DeputatPage'),
+  },
+  {
     path: '/*',
     redirect: '/',
   },
