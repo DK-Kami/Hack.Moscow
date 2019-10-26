60c68e694889eaa09b2f21ec70e3de2b8ac3e8a9
Kami San <1.dankon.1@gmail.com>
2019-10-26 15:57:06 +0300

    add region page

--- src/views/RegionsPage/Map.vue
+++ src/views/RegionsPage/Map.vue
@@ -7,7 +7,6 @@
   >
     <yandex-map
       :controls="['zoomControl']"
-      :settings="settings"
       :coords="coords"
       style="width: 100%; max-width: 100%; height: 100%;"
       zoom="4"
@@ -24,76 +23,74 @@ export default {
   name: 'Map',
   components: { yandexMap, ymapMarker },
   props: {
-    objects: [Array, Object],
+    country: String,
   },
   data: () => ({
     coords: [61.52401, 105.318756],
     objectManager: null,
-    objectCompanyManager: null,
     ymaps: null,
     map: null,
-
-    settings: {
-      restrictMapArea: [[10, 10], [85,-160]],
-    },
   }),
   methods: {
     mapInit(map) {
       this.map = map;
       this.objectManager = new ymaps.ObjectManager();
       this.ymaps = ymaps;
-    },
 
-    async initMarkers() {
-      this.objectCompanyManager = new ymaps.ObjectManager();
+      ymaps.borders.load(this.country, {
+        lang: 'ru',
+        quality: 3
+      })
+        .then(async result => {
+          // const regions = (await axios.get('http://localhost:8000/api/regions')).data;
+          console.log(result);
 
-      const features = this.objects.map(async object => {
-        const [projectNameRus, projectTypeRus, commodityRus, reportingCompanyRus] = await this.getTranslateObject(object);
+          result.features.forEach(feature => {
+            var iso = feature.properties.iso3166;
+            feature.id = iso;
+            // const data = regions.find(r => r.region === feature.properties.name);
 
-        return {
-          type: "Feature",
-          id: object._id,
-          options: {
-            id: object._id,
-            iconLayout: "default#image",
-            iconImageHref: this.currentImage(object.projectType)
-          },
-          geometry: {
-            type: "Point",
-            coordinates: [+object.lat, +object.lon],
-          },
-          properties: {
-            balloonContentHeader: projectNameRus,
-            balloonContentBody:
-            `
-              <div>Класификация:  ${projectTypeRus}</div>
-              <div>Продукция:	${commodityRus}</div>
-              <div>Выручка:	${Number(parseFloat(object.payment).toFixed(2)).toLocaleString()}</div>
-              <div>Компания:	${reportingCompanyRus}</div>
-            `,
-            reportingCompany: object.projectTypeRus,
-            commodity: object.commodityRus,
-            type: object.paymentRus,
-            name: object.reportingCompanyRus,
-            payment: object.payment,
-          }
-        };
-      });
-      console.log(features);
-      Promise.all(features)
-        .then(res => {
-          const featuresCollection = {
-            type: "FeatureCollection",
-            features: res,
-          };
+            // feature.properties.balloonContentHeader = feature.properties.name;
+            // if (data) {
+            //   const template =
+            //   `
+            //     <div>Налог нефтегаза: ${data.nalog_ng}</div>
+            //     <div>Доход по акцизам (Дизель): ${data.income_akz_dizel}</div>
+            //     <div>Доход по акцизам (Нефть): ${data.income_akz_machin_oil}</div>
+            //     <div>Доход по акцизам (Бензин): ${data.income_akz_macine_benz}</div>
+            //   `;
+            //   if (!!Number(data.akz_machine_benz)) {
+            //     template += `<div>Акцизы (Бензин): ${data.akz_machine_benz}</div>`
+            //   }
+            //   if (!!Number(data.akz_dizel)) {
+            //     template += `<div>Акцизы (Дизель): ${data.akz_dizel}</div>`
+            //   }
+            //   if (!!Number(data.akz_motor_oil)) {
+            //     template += `<div>Акцизы (Машинное масло): ${data.akz_motor_oil}</div>`
+            //   }
+            //   feature.properties.balloonContentBody = template;
+            // }
 
-          this.objectCompanyManager.add(featuresCollection);
-          this.map.geoObjects.add(this.objectCompanyManager);
-        })
+            feature.options = {
+              fillColor: '#000000',
+              strokeColor: '#000000',
+              opacity: 0.2,
+              strokeWidth: 2,
+            }
+          });
+
+          this.objectManager.add(result);
+          this.objectManager.objects.events.add('click', this.handleClick);
+          this.map.geoObjects.add(this.objectManager)
+          setTimeout(() => {
+            this.map.setBounds(this.objectManager.getBounds());
+          }, 0);
+        });
     },
 
     handleClick(e) {
       const id = e.get('objectId'),
+        coords = e.get('coords'),
         geoObject = this.objectManager.objects.getById(id);
 
       this.objectManager.objects.each(obj => {
@@ -102,50 +99,19 @@ export default {
         });
       });
 
-      const result = this.ymaps.geoQuery(this.map.geoObjects);
-      this.map.setBounds(this.map.geoObjects.getBounds(), {
-        checkZoomRange: true
-      });
+      this.map.setCenter(coords);
+      console.log(geoObject);
+      this.$emit('select-region');
     },
-    currentImage(type) {
-      switch(type) {
-        case 'Corporate':
-          return '/img/corporate.png';
-
-        case 'Mining':
-          return '/img/mining.png';
-
-        default:
-          return '/img/oil.png';
-      }
-    },
-
-    async getTranslateObject(text) {
-      const {
-        projectName,
-        projectType,
-        commodity,
-        reportingCompany,
-      } = text;
-
-      const result = await axios.get(`
-        https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191020T100434Z.395ba249d0dd7507.6ab300ad2ed430ce6444bd9382274f117ef580d6&text=${projectName}&text=${projectType}&text=${commodity}&text=${reportingCompany[0]}&lang=en-ru
-      `);
-      
-      if (result.data.text[1].trim() === 'Корпоративные') result.data.text[1] = 'Коропрация';
-      if (result.data.text[1].trim() === 'Масло') result.data.text[1] = 'Нефть';
-      if (result.data.text[2].trim() === 'масло') result.data.text[2] = 'Нефть';
-      if (result.data.text[2].trim() === 'лесоматериалами') result.data.text[2] = 'Лесоматериалы';
-
-      return result.data.text;
-    }
   },
   watch: {
-    objects(neVal) {
-      if (this.objectCompanyManager) {
-        this.objectCompanyManager.removeAll();
+    country() {
+      if (this.map) {
+        if (this.objectManager) {
+          this.objectManager.removeAll();
+        }
+        this.mapInit(this.map);
       }
-      this.initMarkers();
     },
   },
 };
--- src/views/RegionsPage/index.vue
+++ src/views/RegionsPage/index.vue
@@ -1,61 +1,28 @@
 <template>
   <v-layout>
+    <v-layout style="position: fixed; z-index: 10000" class="mt-8">
+      <v-flex class="pa-4">
+        <v-layout class="headline" align-end>
+          <select-primary
+            v-model="selectedCountry"
+            :items="countries"
+            title-class="display-1"
+            item-value="code"
+            item-text="name"
+            with-border
+          />
+        </v-layout>
+      </v-flex>
+    </v-layout>
+
     <v-flex xs7>
-      <map-view :objects="currentObjects" />
+      <map-view :country="selectedCountry" />
     </v-flex>
 
     <v-flex class="sider--left">
       <v-container class="px-10 mt-8">
         <v-layout wrap>
-          <v-flex xs8>
-            <v-autocomplete
-              v-model="selectedCompany"
-              :items="values"
-              label="Выбор компании"
-              item-value="value"
-              item-text="text"
-            />
-          </v-flex>
-
-          <v-layout column v-if="company">
-            <v-flex>
-              <div class="display-2">{{ getRussianName(company.companyDetail.companyName) }}</div>
-              <div class="subtitle">
-                <a :href="company.companyDetail.website" class="text--secondary">Website компании</a>
-              </div>
-            </v-flex>
-
-            <v-flex class="mt-8">
-              <div class="display-1 mb-4 font-weight-light">График доходов</div>
-              <v-layout v-if="company.entity.length" justify-center>
-                <v-flex xs9>
-                  <line-chart :chart-data="currentCollection(company.entity)"></line-chart>
-                </v-flex>
-              </v-layout>
-              <div v-else>Данных нет</div>
-            </v-flex>
-
-            <v-flex class="mt-8">
-              <div class="display-1 mb-4 font-weight-light">Даные о доходах</div>
-              <div>
-                <v-data-table
-                  :items="company.entity"
-                  :headers="headers"
-                >
-                  <template #item.sourceURL="{ item }">
-                    <a :href="item.sourceURL">Просмотреть</a>
-                  </template>
-                </v-data-table>
-              </div>
-            </v-flex>
-
-            <v-flex class="mt-8">
-              <div class="display-1 mb-4 font-weight-light">Отчёты</div>
-              <div v-for="report in company.companyDetail.reportYear">
-                <a :href="report.link">Отчёт за {{ report.year }} год</a>
-              </div>
-            </v-flex>
-          </v-layout>
+          here
         </v-layout>
       </v-container>
     </v-flex>
@@ -63,113 +30,24 @@
 </template>
 
 <script>
-import LineChart from '@/components/LineChart.js'
+import SelectPrimary from '@/components/SelectPrimary';
 import MapView from './Map';
 import axios from 'axios';
-
-const headers = [
-  { value: 'reportYear', text: 'Год' },
-  { value: 'country', text: 'Страна' },
-  { value: 'payment', text: 'Прибыль' },
-  { value: 'paymentType', text: 'Прибль за' },
-  { value: 'sourceURL', text: 'Документ' },  
-];
 
 export default {
   name: 'RegionsPage',
   components: {
-    LineChart,
+    SelectPrimary,
     MapView,
   },
-  created() {
-    // this.initMarkers();
-  },
   data: () => ({
-    selectedCompany: 'PAO Gazprom',
-    russianName: 'ПАО Газпром',
-    objects: [],
-    values: [],
-
-    company: null,
-    headers,
+    selectedCountry: 'RU',
   }),
   computed: {
-    currentObjects() {
-      return this.objects.filter(o => o.reportingCompany[0] === this.selectedCompany);
+    countries() {
+      return this.$store.getters.getCountries;
     },
   },
-  methods: {
-    currentCollection(data) {
-      const labels = [];
-      data.forEach(d => {
-        if (!labels.includes(d.reportYear)) labels.push(d.reportYear);
-      });
-
-      const datasets = [{
-        label: 'Доход компании за весь период',
-        backgroundColor: '#5cc0eb',
-        data: [],
-      }];
-
-      labels.forEach(year => {
-        const price = data.reduce((summ, current) => {
-          return current.reportYear === year
-            ? summ + current.payment
-            : summ;
-        }, 0);
-
-        datasets[0].data.push(Number(parseFloat(price).toFixed(2)));
-      })
-
-      return { labels, datasets };
-    },
-
-    getRussianName(value) {
-      return this.values.find(v => v.value === value).text;
-    },
-
-    async initMarkers() {
-      this.objects = (await axios.get('get_country')).data.result[1];
-      console.log(this.objects);
-
-      this.rus = (await this.getTranslate(this.objects.map(o => o.reportingCompany[0]).join('|')))[0].split('|');
-
-      this.values = [];
-      this.objects.forEach((item, index) => {
-        this.values.push({
-          value: item.reportingCompany[0],
-          text: this.rus[index],
-        })
-      });
-      this.loadCompanyData();
-
-      // const companiesRus = (await this.getTranslate(this.objects.map(o => o.reportingCompany[0]).join('|'))).split('|');
-
-      // companiesRus.forEach((rus, index) => {
-      //   console.log(rus);
-      //   this.values.push({
-      //     value: this.objects[index].reportingCompany[0],
-      //     text: rus,
-      //   });
-      // });
-    },
-    async getTranslate(text) {
-      const result = await axios.get(`
-        https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191020T092535Z.f3b868825bc76bde.94c2c40e5741768db6a07640e28c83592b7d065a&text=${text}&lang=en-ru
-      `);
-
-      return result.data.text;
-    },
-    async loadCompanyData() {
-      this.company = (await axios.get('get_company' + this.selectedCompany.split(' ').join('+'))).data;
-      console.log(this.company);
-    }
-  },
-  watch: {
-    selectedCompany() {
-      this.loadCompanyData();
-    }
-  }
 };
 </script>
 
